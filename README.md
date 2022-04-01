d26c61304c7d2f6654c180ea92dbb72c65246e2f
zendus <uzendujmike@gmail.com>
2022-04-02 00:29:48 +0100

    first push

---
+++ README.md
@@ -0,0 +1,15 @@
+# 🏗 scaffold-eth | 🏰 BuidlGuidl
+
+## 🚩 Challenge 2: 🏵 Token Vendor 🤖
+
+## Surge Link
+
+[https://z-vendor.surge.sh/](https://z-vendor.surge.sh/)
+
+## Etherscan link for Vendor
+
+[https://rinkeby.etherscan.io/address/0x6C5D45b97c2D5945991A62f0bA8Bf3Aed7179E4B](https://rinkeby.etherscan.io/address/0x6C5D45b97c2D5945991A62f0bA8Bf3Aed7179E4B)
+
+## Etherscan link for Your token
+
+[https://rinkeby.etherscan.io/address/0x69e8D8a15B6301304d9e2b169053d7e5B8D08d4F](https://rinkeby.etherscan.io/address/0x69e8D8a15B6301304d9e2b169053d7e5B8D08d4F)
---
+++ packages/hardhat/contracts/Vendor.sol
@@ -0,0 +1,44 @@
+pragma solidity ^0.8.4;
+// SPDX-License-Identifier: MIT
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "./YourToken.sol";
+
+contract Vendor is Ownable {
+
+  event BuyTokens(address buyer, uint256 amountOfETH, uint256 amountOfTokens);
+  event SellTokens(address seller, uint256 amountOfTokens, uint256 amountOfETH);
+
+  YourToken public yourToken;
+
+  uint256 public constant tokensPerEth = 100;
+
+  constructor(address tokenAddress) {
+    yourToken = YourToken(tokenAddress);
+  }
+
+  // ToDo: create a payable buyTokens() function:
+  function buyTokens() public payable {
+    uint amountOfTokens = tokensPerEth * msg.value;
+    yourToken.transfer(msg.sender, amountOfTokens);
+    emit BuyTokens(msg.sender, msg.value, amountOfTokens);
+  }
+
+  // ToDo: create a withdraw() function that lets the owner withdraw ETH
+  function withdraw() public onlyOwner {
+    uint256 amount = address(this).balance;
+    address owner = msg.sender;
+    require(amount > 0, "No ETH in Vendor");
+    (bool success,) = owner.call{value: amount}("");
+    require(success, "Failed to send Ether");
+
+  }
+
+  // ToDo: create a sellTokens() function:
+  function sellTokens(uint256 amount) public {
+    uint256 amountToSell = amount / tokensPerEth;
+    yourToken.transferFrom(msg.sender, address(this), amount);
+    (bool sent, bytes memory data) = msg.sender.call{value: amountToSell}("");
+    emit SellTokens(msg.sender, amount, amountToSell);
+  }
+}
